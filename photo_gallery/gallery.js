080d20c36dd611caee314c8fbe281cd6f7379c7d
dk808080 <57583077+dk808080@users.noreply.github.com>
2020-01-27 19:23:17 +0530

    Update gallery.js

--- photo_gallery/gallery.js
+++ photo_gallery/gallery.js
@@ -2,12 +2,12 @@
 
 function upDate(previewPic){
 
- document.getElementbyId("image").style.backgroundImage:url"("+previewPic.src+")";
-document.getElementbyId("image").innerHTML=previewPic.alt;
+ document.getElementById("image").style.backgroundImage="url("+previewPic.src+")";
+document.getElementById("image").innerHTML=previewPic.alt;
 	}
 
 	function unDo(){
-     document.getElementbyId("image").style.backgroundImage:url("");
-   document.getElementbyId("image").innerHTML="Hover over an image below to display here.";
+     document.getElementById("image").style.backgroundImage=url("");
+   document.getElementById("image").innerHTML="Hover over an image below to display here.";
 
 	}
